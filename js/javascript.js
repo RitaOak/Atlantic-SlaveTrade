8bf2c3ea31eea0b381210cf1566155bd9332061a
Beatriz Marques <Beatriz@Beatrizs-MacBook-Pro-2.local>
2018-05-24 18:10:18 +0100

    funcionamente botões coluna esquerda

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,41 @@
-$('a').on('click', function(){
-    $(this).addClass('.button_selected');
-    console.log("entrou");
+$(document).ready(function () {
+
+    /* PRIMEIRO BOTÃO */
+    $("#total_desem").on('click', function () {
+        if ($(this).hasClass("button_selected")) {
+            return "already has";
+          } else {
+            $(this).addClass("button_selected");
+            $("#total_em").removeClass("button_selected");
+          }
+    });
+    
+    $("#total_em").on('click', function () {
+        if ($(this).hasClass("button_selected")) {
+            return "already has";
+          } else {
+            $(this).addClass("button_selected");
+            $("#total_desem").removeClass("button_selected");
+          }
+    });
+
+
+    /* SEGUNDO BOTÃO */
+    $("#percentagem_homens").on('click', function () {
+        if ($(this).hasClass("button_selected")) {
+            return "already has";
+          } else {
+            $(this).addClass("button_selected");
+            $("#percentagem_criancas").removeClass("button_selected");
+          }
+    });
+
+    $("#percentagem_criancas").on('click', function () {
+        if ($(this).hasClass("button_selected")) {
+            return "already has";
+          } else {
+            $(this).addClass("button_selected");
+            $("#percentagem_homens").removeClass("button_selected");
+          }
+    });
 });
